bc501ede7260793bcc8a375aa41eae38d8e2828d
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2024-06-04 13:03:47 +0200

    imported fa icons and fixed responsive navbar

--- react-portfolio/src/components/Header.jsx
+++ react-portfolio/src/components/Header.jsx
@@ -1,16 +1,26 @@
 import github from "../assets/github.svg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { useRef } from "react";
 
 const Header = () => {
+  const navRef = useRef();
+
+  const showNavBar = () => {
+    navRef.current.classList.toggle('active')
+  }
+
   return (
     <nav>
       <h1 className="logo">Leo</h1>
-      <ul>
+      <ul ref={navRef} className="navLinks">
         <li>About</li>
         <li>Past Work</li>
         <li>Skills</li>
         <li>Contact</li>
       </ul>
-      <img src={github} alt="github logo" />
+      <img src={github} alt="github logo" className="github-logo"/>
+      <FontAwesomeIcon icon={faBars} className="burger" onClick={showNavBar}/>
     </nav>
   );
 }
