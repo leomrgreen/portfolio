b994d36c663f71be33edf85d1d9679543ba7406d
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2024-06-07 14:13:17 +0200

    added contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,43 @@
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from 'react';
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import chatimg from "../images/Chat-pana.png";
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Contact = () => {
+    const [buttonText, setButtonText] = useState('Copy My Gmail');
+
+    const copyEmailToClipboard = () => {
+        navigator.clipboard.writeText("jonssonleonardo@gmail.com")
+            .then(() => {
+                setButtonText('Copied!');
+            })
+            .catch(err => {
+                console.error('Error copying text: ', err);
+            });
+    }
+
     return (
-        <section>
+        <section className='contact-section'>
             <div className="contact-title">
                 <h2>Contact</h2>
                 <FontAwesomeIcon icon={faPhone} />
+            </div>
+            <div className="contact-card">
+                <img src={chatimg} alt="man standing next to a big phone" />
+                <div className="btn-container-contact">
+                    <button onClick={copyEmailToClipboard}>
+                        {buttonText} 
+                        <FontAwesomeIcon icon={faEnvelope} />
+                    </button>
+                    <div className="media"> 
+                        <a href="https://github.com/leomrgreen"> <FontAwesomeIcon icon={faGithub} /> </a>
+                        <a href="https://www.linkedin.com/in/leo-jonsson-b98048304/"> <FontAwesomeIcon icon={faLinkedin} /> </a>
+                    </div>
+                </div>
             </div>
         </section>
     );
 }
 
-export default Contact
+export default Contact;
