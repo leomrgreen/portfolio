dee40173d3825ba9f515dda968fbd47a71677176
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2024-06-10 18:27:50 +0200

    added contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import chatimg from "../images/Chat-pana.png";
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
     const [buttonText, setButtonText] = useState('Copy My Gmail');
+    const formRef = useRef();
 
     const copyEmailToClipboard = () => {
         navigator.clipboard.writeText("jonssonleonardo@gmail.com")
@@ -17,26 +20,52 @@ const Contact = () => {
             });
     }
 
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        const serviceID = import.meta.env.VITE_SERVICE_ID;
+        const templateID = import.meta.env.VITE_TEMPLATE_ID;
+        const publicKey = import.meta.env.VITE_PUBLIC_KEY;
+
+        emailjs.sendForm(serviceID, templateID, formRef.current, publicKey)
+            .then((result) => {
+                console.log(result.text);
+                alert('Email sent!')
+                formRef.current.reset();  
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
+
     return (
-        <section className='contact-section hidden' id='contact-section'>
-            <div className="contact-title">
-                <h2>Contact</h2>
-                <FontAwesomeIcon icon={faPhone} />
-            </div>
-            <div className="contact-card">
-                <img src={chatimg} alt="man standing next to a big phone" className='hidden'/>
-                <div className="btn-container-contact hidden">
-                    <button onClick={copyEmailToClipboard}>
-                        {buttonText} 
-                        <FontAwesomeIcon icon={faEnvelope} />
-                    </button>
-                    <div className="media"> 
-                        <a href="https://github.com/leomrgreen"> <FontAwesomeIcon icon={faGithub} /> </a>
-                        <a href="https://www.linkedin.com/in/leo-jonsson-b98048304/"> <FontAwesomeIcon icon={faLinkedin} /> </a>
+        <>
+            <section className='contact-section hidden' id='contact-section'>
+                <div className="contact-title">
+                    <h2>Contact</h2>
+                    <FontAwesomeIcon icon={faPhone} />
+                </div>
+                <div className="contact-card">
+                    <form className='contact-form' onSubmit={sendEmail} ref={formRef}>
+                        <label htmlFor="emailFrom">Your Email:</label>
+                        <input type="text" name='email_from' id='emailFrom' className='email-from' placeholder='person@example.com'/>
+                        <label htmlFor="message">Message:</label>
+                        <textarea name="message" id="message" rows={10} className='message-box' placeholder='Write me something...'></textarea>
+                        <button type='submit' className='send-btn'>SEND <FontAwesomeIcon icon={faPaperPlane} /></button>
+                    </form>
+                    <img src={chatimg} alt="man standing next to a big phone" className='hidden'/>
+                    <div className="btn-container-contact hidden">
+                        <button onClick={copyEmailToClipboard}>
+                            {buttonText} 
+                            <FontAwesomeIcon icon={faEnvelope} />
+                        </button>
+                        <div className="media"> 
+                            <a href="https://github.com/leomrgreen"> <FontAwesomeIcon icon={faGithub} /> </a>
+                            <a href="https://www.linkedin.com/in/leo-jonsson-b98048304/"> <FontAwesomeIcon icon={faLinkedin} /> </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     );
 }
 
