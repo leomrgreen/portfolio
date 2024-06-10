da425c6221d36e66b1ce5e8169546e5bd6c744aa
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2024-06-10 16:46:08 +0200

    added react-icons & darkMode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { useRef, useState } from "react";
+import DarkMode from './DarkMode';
 
 const Header = () => {
   const navRef = useRef();
@@ -16,13 +17,16 @@ const Header = () => {
     <nav id='nav'>
       <a href="/" className="logo"><h1>Leo</h1></a>
       <ul ref={navRef} className="navLinks">
-        <li><a href="#about-section" onClick={showNavBar}>About</a></li>
+        <li><a href="#about-section" onClick={showNavBar} className='testar'>About</a></li>
         <li><a href="#skills" onClick={showNavBar}>Skills</a></li>
         <li><a href="#projects" onClick={showNavBar}>Projects</a></li>
         <li><a href="#contact-section" onClick={showNavBar}>Contact</a></li>
       </ul>
+     <div className='nav-right'>
+     <DarkMode />
       <a href="https://github.com/leomrgreen" className='github-logo'><FontAwesomeIcon icon={faGithub} /></a>
-      <FontAwesomeIcon
+     </div>
+     <FontAwesomeIcon
         icon={isNavActive ? faXmark : faBars}
         className="burger"
         onClick={showNavBar}
