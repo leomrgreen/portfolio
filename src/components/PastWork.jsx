14c910cf03f2c9103be243cde676388430c8d42e
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2024-06-13 12:19:46 +0200

    css updates

--- src/components/PastWork.jsx
+++ src/components/PastWork.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import projects from "../data/projects";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCode, faCodeFork } from "@fortawesome/free-solid-svg-icons";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { TbFolderCode } from "react-icons/tb";
+
 
 const Portfolio = () => {
     return (
         <>
             <section className="project-section" id="projects">
                <div className="projects-title">
-               <h2 className="hidden">Projects</h2> <FontAwesomeIcon icon={faCodeFork} className="fork" />
+               <h2 className="hidden">Projects</h2> <TbFolderCode  className="folder-code" />
                </div>
                 <div className="project-grid">
                 {projects.map((project, index) => (
