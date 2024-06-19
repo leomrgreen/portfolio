b053651afd4cfa873a292390aa3c24101ed8d322
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2024-06-19 19:58:51 +0200

    added modal pop-up function in projects-section

---
+++ src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FaXmark } from 'react-icons/fa6';
+
+const Modal = ({ project, onClose }) => {
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <h1 className='modal-title'>{project.title}</h1>
+        <span className="close-modal" onClick={onClose}><FaXmark className='close-icon'/></span>
+        <img src={project.image.url} alt={project.image.alt} />
+        <div className="modal-desc">
+            <p>{project.description}</p>
+            <span>Built with:</span>
+            <div className='languages'>
+            {project.tags.map((tag, tagIndex) => (
+            <span className="tags" key={tagIndex}>{tag}</span>))}
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
--- src/components/PastWork.jsx
+++ src/components/PastWork.jsx
@@ -1,34 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import projects from "../data/projects";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Modal from "./Modal";
 import { faCode } from "@fortawesome/free-solid-svg-icons";
 import { TbFolderCode } from "react-icons/tb";
 
-
 const Portfolio = () => {
-    return (
-        <>
-            <section className="project-section" id="projects">
-               <div className="projects-title">
-               <h2 className="hidden">Projects</h2> <TbFolderCode  className="folder-code" />
-               </div>
-                <div className="project-grid">
-                {projects.map((project, index) => (
-                <div className="project-card hidden" key={index}>
-                    <h4>{project.title}</h4>
-                    <span>{project.description}</span>
-                    <img src={project.image.url} alt={project.image.alt} />
-                    <div className="project-tags-container">
-                     {project.tags.map((tag, tagIndex) => (
-                     <span className="tags" key={tagIndex}>{tag}</span>))}
-                     </div>
-                    <a href={project.github} target="_blank" className="src-code"><FontAwesomeIcon icon={faCode} /> </a>
-                </div>
-            ))}
-                </div>
-            </section>
-        </>
-    );
-}
+  const [selectedProject, setSelectedProject] = useState(null);
 
-export default Portfolio
+  function openModal(project) {
+    setSelectedProject(project);
+  }
+
+  function closeModal() {
+    setSelectedProject(null);
+  }
+
+  return (
+    <section className="project-section" id="projects">
+      <div className="projects-title">
+        <h2 className="hidden">Projects</h2>
+        <TbFolderCode className="folder-code" />
+      </div>
+      <div className="project-grid">
+        {projects.map((project, index) => (
+          <div className="project-card hidden" key={index}>
+            <h4>{project.title}</h4>
+            <span>{project.description}</span>
+            <img 
+              src={project.image.url} 
+              alt={project.image.alt} 
+              onClick={() => openModal(project)} 
+            />
+            <div className="project-tags-container">
+              {project.tags.map((tag, tagIndex) => (
+                <span className="tags" key={tagIndex}>{tag}</span>
+              ))}
+            </div>
+            <a href={project.github} target="_blank" className="src-code">
+              <FontAwesomeIcon icon={faCode} />
+            </a>
+          </div>
+        ))}
+      </div>
+      {selectedProject && (
+        <Modal project={selectedProject} onClose={closeModal} />
+      )}
+    </section>
+  );
+};
+
+export default Portfolio;
