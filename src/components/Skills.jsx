5c90e9f539d9fe5638cf172fdb3fa553da3dd5a9
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2024-07-03 14:15:46 +0200

    r

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -20,82 +20,79 @@ const Skills = () => {
   };
 
   return (
-    <>
-      <div className="skill-gradient"></div>
-      <section className="tech-stack" id="skills">
-        <div className="skills-title">
-          <h2 className="skills-title">SKILLS</h2>
-          <FontAwesomeIcon icon={faCode} />
+    <section className="tech-stack" id="skills">
+      <div className="skills-title">
+        <h2 className="skills-title">SKILLS</h2>
+        <FontAwesomeIcon icon={faCode} />
+      </div>
+      <div className="skill-grid left hidden">
+        <div className="skill-card response">
+          <span className="response-title">RESPONSIVE WEB DESIGN</span>
+          <img src={responsive} alt="illustration of a responsive website" />
         </div>
-        <div className="skill-grid left hidden">
-          <div className="skill-card response">
-            <span className="response-title">RESPONSIVE WEB DESIGN</span>
-            <img src={responsive} alt="illustration of a responsive website" />
+        <div className="skill-card right hidden">
+          <span className="comp-title">Building Components</span>
+          <img
+            src={tailwind_component}
+            alt="tailwind component illustration"
+            className="tailwind-comp"
+          />
+        </div>
+        <div className="skill-card robot-card hidden">
+          <div className="robot">
+            <img src={approach} alt="robot" className="robot-img" />
           </div>
-          <div className="skill-card right hidden">
-            <span className="comp-title">Building Components</span>
+          <div className="approach">
+            <span className="approach-title">APPROACH</span>
+            <ul className="approach-list">
+              <li className="list-item">
+                <FontAwesomeIcon icon={faCheckCircle} className="checkMark" />{" "}
+                Design
+              </li>
+              <li className="list-item">
+                <FontAwesomeIcon icon={faCheckCircle} className="checkMark" />{" "}
+                Develop
+              </li>
+              <li className="list-item">
+                <FontAwesomeIcon icon={faCheckCircle} className="checkMark" />{" "}
+                Deploy
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="skill-card right hidden">
+          <span className="tech-title">My Tech Stack</span>
+          <div className="bento-grid">
+            {techStack.map((stack, index) => (
+              <span key={index} className="stack">
+                {stack}
+              </span>
+            ))}
+          </div>
+        </div>
+        <div className="skill-card dark-mode-skill">
+          <span className="comp-title">Dark Mode</span>
+          <div className="day-night-toggle">
             <img
-              src={tailwind_component}
-              alt="tailwind component illustration"
-              className="tailwind-comp"
+              src={isDay ? dayTime : nightTime}
+              alt="Day or Night illustration"
+              className="day-night"
             />
-          </div>
-          <div className="skill-card robot-card hidden">
-            <div className="robot">
-              <img src={approach} alt="robot" className="robot-img" />
-            </div>
-            <div className="approach">
-              <span className="approach-title">APPROACH</span>
-              <ul className="approach-list">
-                <li className="list-item">
-                  <FontAwesomeIcon icon={faCheckCircle} className="checkMark" />{" "}
-                  Design
-                </li>
-                <li className="list-item">
-                  <FontAwesomeIcon icon={faCheckCircle} className="checkMark" />{" "}
-                  Develop
-                </li>
-                <li className="list-item">
-                  <FontAwesomeIcon icon={faCheckCircle} className="checkMark" />{" "}
-                  Deploy
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="skill-card right hidden">
-            <span className="tech-title">My Tech Stack</span>
-            <div className="bento-grid">
-              {techStack.map((stack, index) => (
-                <span key={index} className="stack">
-                  {stack}
-                </span>
-              ))}
-            </div>
-          </div>
-          <div className="skill-card dark-mode-skill">
-            <span className="comp-title">Dark Mode</span>
-            <div className="day-night-toggle">
-              <img
-                src={isDay ? dayTime : nightTime}
-                alt="Day or Night illustration"
-                className="day-night"
-              />
-              <button onClick={toggleDayNight} className="day-or-night">
-                {!isDay ? (
-                  <FiSun className="sun-yel" />
-                ) : (
-                  <BsFillMoonStarsFill className="moon-pur" />
-                )}
-              </button>
-            </div>
-          </div>
-          <div className="skill-card carousel-card">
-            <span className="comp-title">Responsive Carousel</span>
-            <Carousel data={slides} />
+            <button onClick={toggleDayNight} className="day-or-night">
+              {!isDay ? (
+                <FiSun className="sun-yel" />
+              ) : (
+                <BsFillMoonStarsFill className="moon-pur" />
+              )}
+            </button>
           </div>
         </div>
-      </section>
-    </>
+        <div className="skill-card carousel-card">
+          <span className="comp-title">Responsive Carousel</span>
+          <Carousel data={slides} />
+        </div>
+      </div>
+    </section>
   );
 };
 
